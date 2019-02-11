15ae4547f0449e56401988589ffb2566cefe3aca
Jerome Mottet <mottet.dev@gmail.com>
2019-02-11 22:21:27 +0100

    Add readme and refine script

---
+++ README.md
@@ -0,0 +1,12 @@
+# Real-time Scraping with Python!
+
+Source code for [my tutorial on Medium](https://medium.com/@mottet.dev/real-time-scraping-with-python-5ca773ee473d)
+
+## How to use it
+
+```sh
+git clone
+cd
+pipenv install
+pipenv run python main.py
+```
--- main.py
+++ main.py
@@ -1,36 +1,62 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib import parse
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
 
-r = requests.get('https://store.steampowered.com/search/?term=The+witcher')
+app = Flask(__name__)
+api = Api(app)
 
-soup = BeautifulSoup(r.text, 'html.parser')
 
-resultsRow = soup.find_all('a', {'class': 'search_result_row'})
+class SteamSearch(Resource):
+    def put(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('term', required=True,
+                            help='A search term needs to be provided')
+        args = parser.parse_args()
 
-results = []
+        formattedSearchTerm = parse.urlencode({'term': args.term})
 
-for resultRow in resultsRow:
-    gameURL = resultRow.get('href')
-    title = resultRow.find('span', {'class': 'title'}).text
-    releaseDate = resultRow.find('div', {'class': 'search_released'}).text
-    imgURL = resultRow.select('div.search_capsule img')[0].get('src')
-    price = None
-    discountedPrice = None
-    if (resultRow.select('div.search_price span strike')):
-        price = resultRow.select('div.search_price span strike')[
-            0].text.strip(' \t\n\r')
-        if (resultRow.select('div.search_price')):
-            rawDiscountPrice = resultRow.select(
-                'div.search_price')[0].text.strip(' \t\n\r')
-            discountedPrice = rawDiscountPrice.replace(price, '')
+        r = requests.get(
+            f'https://store.steampowered.com/search/?{formattedSearchTerm}')
 
-    results.append({
-        'gameURL': gameURL,
-        'title': title,
-        'releaseDate': releaseDate,
-        'imgURL': imgURL,
-        'price': price,
-        'discountedPrice': discountedPrice
-    })
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-print(results)
+        resultsRow = soup.find_all('a', {'class': 'search_result_row'})
+
+        results = []
+
+        for resultRow in resultsRow:
+            gameURL = resultRow.get('href')
+            title = resultRow.find('span', {'class': 'title'}).text
+            releaseDate = resultRow.find(
+                'div', {'class': 'search_released'}).text
+            imgURL = resultRow.select('div.search_capsule img')[0].get('src')
+            price = None
+            discountedPrice = None
+
+            if (resultRow.select('div.search_price span strike')):
+                price = resultRow.select('div.search_price span strike')[
+                    0].text.strip(' \t\n\r')
+                if (resultRow.select('div.search_price')):
+                    rawDiscountPrice = resultRow.select(
+                        'div.search_price')[0].text.strip(' \t\n\r')
+                    discountedPrice = rawDiscountPrice.replace(price, '')
+
+            # Once formatted, the data are then appended to the results list
+            results.append({
+                'gameURL': gameURL,
+                'title': title,
+                'releaseDate': releaseDate,
+                'imgURL': imgURL,
+                'price': price,
+                'discountedPrice': discountedPrice
+            })
+
+        return results
+
+
+api.add_resource(SteamSearch, '/steam_search')
+
+if __name__ == '__main__':
+    app.run(debug=True)
